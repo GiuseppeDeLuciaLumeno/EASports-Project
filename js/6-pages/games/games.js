cc78cd1593cf04596d70c83f613f6f1136c406d7
Giuseppe De Lucia Lumeno <giupetrucci@libero.it>
2022-09-25 05:10:26 +0200

    header

--- js/6-pages/games/games.js
+++ js/6-pages/games/games.js
@@ -2,18 +2,22 @@ const modal = document.querySelector(".modal-wrapper");
 const asideShow = document.querySelector(".three-dots-container");
 const asidehide = document.getElementById("icon-x");
 const esc = document.querySelector(".icon-x");
+const allGames = document.querySelector(".all-games-block");
+const cards = document.querySelector(".card-wrapper");
 
+//Aside menu activate with dots
 asideShow.addEventListener("click", () => {
     asideShow.classList.toggle("active");
     modal.showModal();
 });
 
-
+//Aside menu Disactivate with I-icon
 asidehide.addEventListener("click", () => {
    asideShow.classList.remove("active");
    modal.close();
 });
 
+//Aside menu Disactivate width Esc key
 window.addEventListener("keydown", keypress);
   function keypress(key) {
     if (key.keyCode == 27) {
@@ -21,7 +25,11 @@ window.addEventListener("keydown", keypress);
     }
   }
 
+//Aside menu Disactivate by clicking anywhere else
 
+
+
+//Aside menu activate with burger menu
 const burger = document.querySelector(".burger");
 
 burger.addEventListener("click", () => {
@@ -34,7 +42,5 @@ asidehide.addEventListener("click", () => {
 
 
 
-
-//Autofocus:
 
 
