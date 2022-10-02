15069861031c1d87635410a6cd31fefedc8d20d9
DarioCorbinelli <108214071+DarioCorbinelli@users.noreply.github.com>
2022-10-02 14:19:56 +0200

    feat: add functionalities to sidebar

--- js/4-layout/sidebars/sidebar-1-dario.js
+++ js/4-layout/sidebars/sidebar-1-dario.js
@@ -1,6 +1,30 @@
 document.addEventListener('click', e => {
-  if (e.target.closest("aside .dropdown-btn")) {
-    e.target.closest("aside .dropdown").classList.toggle("dropdown-menu-open")
+  if (e.target.closest("aside .dropdown:nth-child(1) .dropdown-btn")) {
+    e.target.closest("aside .dropdown").classList.toggle("giochi-open")
+  }
+  
+  if (e.target.closest("aside .dropdown:nth-child(1) .dropdown-menu .sub-dropdown:nth-child(1) .sub-dropdown-btn")) {
+    e.target.closest("aside .dropdown").classList.toggle("esplora-giochi-open")
+  }
+  
+  if (e.target.closest("aside .dropdown:nth-child(1) .dropdown-menu .sub-dropdown:nth-child(2) .sub-dropdown-btn")) {
+    e.target.closest("aside .dropdown").classList.toggle("piattaforme-open")
+  }
+  
+  if (e.target.closest("aside .dropdown:nth-child(2) .dropdown-btn")) {
+    e.target.closest("aside .dropdown").classList.toggle("altre-esperienze-open")
+  }
+  
+  if (e.target.closest("aside .dropdown:nth-child(3) .dropdown-btn")) {
+    e.target.closest("aside .dropdown").classList.toggle("informazioni-open")
+  }
+  
+  if (e.target.closest("aside .dropdown:nth-child(4) .dropdown-btn")) {
+    e.target.closest("aside .dropdown").classList.toggle("impegni-open")
+  }
+  
+  if (e.target.closest("aside .dropdown:nth-child(5) .dropdown-btn")) {
+    e.target.closest("aside .dropdown").classList.toggle("risorse-open")
   }
 
   if (e.target.closest("aside .sub-dropdown-btn")) {
