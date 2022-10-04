4f9ee53a93b7d3b6f71c81ee6d3b398057a6f17d
DarioCorbinelli <108214071+DarioCorbinelli@users.noreply.github.com>
2022-10-04 11:51:03 +0200

    feat: complete sidebars part

--- js/4-layout/navbars/navbar-1-dario.js
+++ js/4-layout/navbars/navbar-1-dario.js
@@ -1,60 +1,48 @@
-// HIDE & SHOW navbar on scroll down/up ------------------------------------------------------------
+const body = document.querySelector("body");
+const darkSection = document.querySelector(".dark-section");
+const lightSection = document.querySelector(".light-section");
+const asideMobile = document.querySelector(".aside-mobile");
+const asideDesktop = document.querySelector(".aside-desktop");
+const toggle = document.querySelector(".toggle");
 
-let navbar = document.querySelector(".navbar");
-let asideMobile = document.querySelector(".aside-mobile");
-let asideDesktop = document.querySelector(".aside-desktop")
 let lastScroll = 0;
-
 document.addEventListener('scroll', () => {
   const currentScroll = window.pageYOffset;
-  
-  if (currentScroll <= 0) {
-    navbar.classList.remove("hidden");
-  }
 
   if (currentScroll > lastScroll) {
-    navbar.classList.add("hidden");
+    darkSection.classList.remove("show");
+    lightSection.classList.remove("show-dark");
   }
   
   if (currentScroll < lastScroll) {
-    navbar.classList.remove("hidden");
+    darkSection.classList.add("show");
+    lightSection.classList.add("show-dark");
   }
 
   lastScroll = currentScroll;
 })
 
-document.addEventListener('click', e => {
+
+let currentScroll;
+let latestScroll;
+document.addEventListener("click", e => {
+  currentScroll = window.pageYOffset;
   if (!asideMobile.classList.contains("active") && e.target.closest(".toggle")) {
+    document.documentElement.style.setProperty("--scroll", -currentScroll + "px");
+    body.classList.add("sidebar-open");
     asideMobile.classList.add("active");
-    navbar.classList.remove("hidden")
-  } else if (asideMobile.classList.contains("active") && window.pageYOffset > 0 && e.target.closest(".aside-mobile") == null) {
-    asideMobile.classList.remove("active");
-    navbar.classList.add("hidden")
-  } else if (asideMobile.classList.contains("active") && window.pageYOffset == 0 && e.target.closest(".aside-mobile") == null) {
-    asideMobile.classList.remove("active");
-  } else if (asideMobile.classList.contains("active") && window.pageYOffset > 0 && e.target.closest(".close-btn")) {
-    asideMobile.classList.remove("active");
-    navbar.classList.add("hidden")
-  } else if (asideMobile.classList.contains("active") && window.pageYOffset == 0 && e.target.closest(".close-btn")) {
-    asideMobile.classList.remove("active");
-  } else if (asideMobile.classList.contains("active") && e.target.closest(".aside-mobile")) {
-    return
-  }
-  
-  if (!asideDesktop.classList.contains("active") && e.target.closest(".toggle")) {
     asideDesktop.classList.add("active");
-    navbar.classList.remove("hidden")
-  } else if (asideDesktop.classList.contains("active") && window.pageYOffset > 0 && e.target.closest(".aside-desktop") == null) {
-    asideDesktop.classList.remove("active");
-    navbar.classList.add("hidden")
-  } else if (asideDesktop.classList.contains("active") && window.pageYOffset == 0 && e.target.closest(".aside-desktop") == null) {
-    asideDesktop.classList.remove("active");
-  } else if (asideDesktop.classList.contains("active") && window.pageYOffset > 0 && e.target.closest(".close-btn")) {
-    asideDesktop.classList.remove("active");
-    navbar.classList.add("hidden")
-  } else if (asideDesktop.classList.contains("active") && window.pageYOffset == 0 && e.target.closest(".close-btn")) {
-    asideDesktop.classList.remove("active");
-  } else if (asideDesktop.classList.contains("active") && e.target.closest(".aside-desktop")) {
-    return
+    latestScroll = currentScroll;
+  }
+})
+
+document.addEventListener("click", e => {
+  if (asideMobile.classList.contains("active")) {
+    if (e.target.closest(".close-btn") || e.target.closest(".overlay")) {
+      body.classList.remove("sidebar-open");
+      asideMobile.classList.remove("active");
+      asideDesktop.classList.remove("active");
+      document.documentElement.scrollTop = latestScroll;
+    }
   }
 })
