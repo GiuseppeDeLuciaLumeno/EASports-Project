b777637c4bef5f05fe3c7f881adbe28389c7c058
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-11-02 12:18:39 +0100

    fix: make navbar and sidebar 'bootstrap compatible'

--- js/4-layout/sidebars/sidebar-1-dario.js
+++ js/4-layout/sidebars/sidebar-1-dario.js
@@ -1,35 +1,35 @@
 let sidebar = document.querySelector(".aside-mobile");
 
 document.addEventListener('click', e => {
-  if (e.target.closest(".aside-mobile .dropdown:nth-child(1) .dropdown-btn")) {
-    e.target.closest(".aside-mobile .dropdown").classList.toggle("giochi-open")
+  if (e.target.closest(".aside-mobile .da-dropdown:nth-child(1) .dropdown-btn")) {
+    e.target.closest(".aside-mobile .da-dropdown").classList.toggle("giochi-open")
   }
 
-  if (e.target.closest(".aside-mobile .dropdown:nth-child(1) .dropdown-menu .sub-dropdown:nth-child(1) .sub-dropdown-btn")) {
-    e.target.closest(".aside-mobile .dropdown").classList.toggle("esplora-giochi-open")
+  if (e.target.closest(".aside-mobile .da-dropdown:nth-child(1) .da-dropdown-menu .da-sub-dropdown:nth-child(1) .sub-dropdown-btn")) {
+    e.target.closest(".aside-mobile .da-dropdown").classList.toggle("esplora-giochi-open")
   }
 
-  if (e.target.closest(".aside-mobile .dropdown:nth-child(1) .dropdown-menu .sub-dropdown:nth-child(2) .sub-dropdown-btn")) {
-    e.target.closest(".aside-mobile .dropdown").classList.toggle("piattaforme-open")
+  if (e.target.closest(".aside-mobile .da-dropdown:nth-child(1) .da-dropdown-menu .da-sub-dropdown:nth-child(2) .sub-dropdown-btn")) {
+    e.target.closest(".aside-mobile .da-dropdown").classList.toggle("piattaforme-open")
   }
 
-  if (e.target.closest(".aside-mobile .dropdown:nth-child(2) .dropdown-btn")) {
-    e.target.closest(".aside-mobile .dropdown").classList.toggle("altre-esperienze-open")
+  if (e.target.closest(".aside-mobile .da-dropdown:nth-child(2) .dropdown-btn")) {
+    e.target.closest(".aside-mobile .da-dropdown").classList.toggle("altre-esperienze-open")
   }
 
-  if (e.target.closest(".aside-mobile .dropdown:nth-child(3) .dropdown-btn")) {
-    e.target.closest(".aside-mobile .dropdown").classList.toggle("informazioni-open")
+  if (e.target.closest(".aside-mobile .da-dropdown:nth-child(3) .dropdown-btn")) {
+    e.target.closest(".aside-mobile .da-dropdown").classList.toggle("informazioni-open")
   }
 
-  if (e.target.closest(".aside-mobile .dropdown:nth-child(4) .dropdown-btn")) {
-    e.target.closest(".aside-mobile .dropdown").classList.toggle("impegni-open")
+  if (e.target.closest(".aside-mobile .da-dropdown:nth-child(4) .dropdown-btn")) {
+    e.target.closest(".aside-mobile .da-dropdown").classList.toggle("impegni-open")
   }
 
-  if (e.target.closest(".aside-mobile .dropdown:nth-child(5) .dropdown-btn")) {
-    e.target.closest(".aside-mobile .dropdown").classList.toggle("risorse-open")
+  if (e.target.closest(".aside-mobile .da-dropdown:nth-child(5) .dropdown-btn")) {
+    e.target.closest(".aside-mobile .da-dropdown").classList.toggle("risorse-open")
   }
 
   if (e.target.closest(".aside-mobile .sub-dropdown-btn")) {
-    e.target.closest(".aside-mobile .sub-dropdown").classList.toggle("sub-dropdown-menu-open")
+    e.target.closest(".aside-mobile .da-sub-dropdown").classList.toggle("sub-dropdown-menu-open")
   }
 })
